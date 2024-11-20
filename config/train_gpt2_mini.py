01303b3a0f6451c3699d3ae96173de5355347e39
Vadimbuildercxx <kurvadimal@gmail.com>
2024-11-20 14:43:28 +0400

    Change config

--- config/train_gpt2_mini.py
+++ config/train_gpt2_mini.py
@@ -3,7 +3,7 @@
 # $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
 
 wandb_log = True
-wandb_project = 'owt'
+wandb_project = 'reflex_attention'
 wandb_run_name='gpt2-mini-6l-5h-emb768'
 
 # these make the total batch size be ~0.5M
@@ -13,8 +13,7 @@ block_size = 512
 gradient_accumulation_steps = 25 * 1
 
 # this makes total number of tokens be 300B
-max_iters = 10000
-lr_decay_iters = 10000
+max_iters = 600
 
 n_layer = 6
 n_head = 6
@@ -22,14 +21,14 @@ n_embd = 768
 dropout = 0.2
 
 # eval stuff
-eval_interval = 1000
-eval_iters = 200
+eval_interval = 50
+eval_iters = 20
 log_interval = 10
 
-learning_rate = 1e-4 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
-min_lr = 1e-5 # learning_rate / 10 usually
+warmup_iters = 50 
+learning_rate = 1e-3 # with baby networks can afford to go a bit higher
+lr_decay_iters = 600 # make equal to max_iters usually
+min_lr = 1e-4 # learning_rate / 10 usually
 
 # weight decay
 weight_decay = 1e-1
