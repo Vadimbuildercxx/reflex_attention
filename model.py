7877bbd4bd6eb957d57e659ca7b19b2a42f08bf5
Vadimbuildercxx <kurvadimal@gmail.com>
2024-11-21 23:08:00 +0400

    Add sampler

--- model.py
+++ model.py
@@ -436,7 +436,7 @@ class GPT(nn.Module):
         return mfu
 
     @torch.no_grad()
-    def generate(self, idx, max_new_tokens, temperature=1.0, top_k=None):
+    def generate(self, idx, max_new_tokens, temperature=1.0, top_k=None, stop=None):
         """
         Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
         the sequence max_new_tokens times, feeding the predictions back into the model each time.
@@ -459,6 +459,8 @@ class GPT(nn.Module):
             idx_next = torch.multinomial(probs, num_samples=1)
             # append sampled index to the running sequence and continue
             idx = torch.cat((idx, idx_next), dim=1)
+            if idx_next == stop:
+                break
 
         return idx
 
